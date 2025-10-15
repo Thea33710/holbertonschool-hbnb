9702739a40310da22ea81d8f00858f6cd6633e2e
Thea33710 <thea33710@gmail.com>
2025-10-15 14:09:22 +0200

    test

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 
 def create_app():
@@ -12,6 +13,7 @@ def create_app():
         doc='/api/v1/'
     )
 
+    api.add_namespace(users_ns, path='/api/v1/users')
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places,
     # reviews, and amenities will be added later
