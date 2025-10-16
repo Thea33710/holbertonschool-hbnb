13c259ca53f2c44caff6a045be3f18b481edc0e8
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-10-16 17:03:29 +0200

    create amenity endpoint

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -10,34 +10,41 @@ amenity_model = api.model('Amenity', {
 
 @api.route('/')
 class AmenityList(Resource):
-    @api.expect(amenity_model)
+    @api.expect(amenity_model, validate=True)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
-        """Register a new amenity"""
-        # Placeholder for the logic to register a new amenity
-        pass
+        """Create a new amenity"""
+        data = api.payload
+        amenity = facade.create_amenity(data)
+        return amenity.to_dict(), 201
 
     @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
-        """Retrieve a list of all amenities"""
-        # Placeholder for logic to return a list of all amenities
-        pass
+        """Get all amenities"""
+        amenities = facade.get_all_amenities()
+        return [a.to_dict() for a in amenities], 200
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
     @api.response(200, 'Amenity details retrieved successfully')
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
-        """Get amenity details by ID"""
-        # Placeholder for the logic to retrieve an amenity by ID
-        pass
+        """Get a specific amenity"""
+        amenity = facade.get_amenity(amenity_id)
+        if not amenity:
+            api.abort(404, "Amenity not found")
+        return amenity.to_dict(), 200
+
 
     @api.expect(amenity_model)
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
-        """Update an amenity's information"""
-        # Placeholder for the logic to update an amenity by ID
-        pass
+        """Update an amenity"""
+        data = api.payload
+        amenity = facade.update_amenity(amenity_id, data)
+        if not amenity:
+            api.abort(404, "Amenity not found")
+        return amenity.to_dict(), 200
